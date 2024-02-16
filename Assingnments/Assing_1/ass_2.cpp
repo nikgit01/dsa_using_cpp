4fba656d7a0f9c2aac1d2e68d176029c5d4dc77d
Nikhil09 <nikhilside72@gmail.com>
2024-02-16 23:44:46 +0530

    repeate hear

---
+++ Assingnments/Assing_1/ass_2.cpp
@@ -0,0 +1,12 @@
+#include "A1st.cpp"
+int greatest(Array obj){
+    int max;
+    if(obj.isEmpty()){
+        cout<<"The array was empty";
+        return -1;
+    }
+    max=obj.get(0);
+    for(int i=1; i<obj.count(); i++){
+
+    }
+}
