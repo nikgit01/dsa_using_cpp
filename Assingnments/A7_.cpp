94af2e83109bc386361adf8862cea94acea60f9a
nikhil09 <nikhilside72@gmail.com>
2024-03-05 00:33:31 +0530

    middle node question of assing 7

--- Assingnments/A7_.cpp
+++ Assingnments/A7_.cpp
@@ -12,7 +12,15 @@ int countList(SLL &s){
     }
     return count;    
 }
-
+node* middleNode(SLL &s) {
+    node *t1, *t2;
+    t1 = t2 = s.getFirstNode();
+    while (t2 != NULL && t2->next != NULL) {
+        t1 = t1->next;
+        t2 = t2->next->next;
+    }
+    return t1;
+}
 //             Main function
 int main()
 {
@@ -22,9 +30,11 @@ int main()
     s1.insertAtLast(30);
     s1.insertAtLast(40);
     s1.insertAtLast(50);
+    s1.insertAtLast(70);
     cout<<"S1: "<<countList(s1)<<endl;
     s1.printList();
-    s1.reverseList();
+    // s1.reverseList();
     s1.printList();
+    cout<<"middle node is : "<<middleNode(s1)<<endl;
     return 0;
 }
