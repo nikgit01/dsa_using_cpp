37fbbe7b1c1d8cec11a1034a06e1f4374218012b
nikhil09 <nikhilside72@gmail.com>
2024-03-08 18:57:58 +0530

    starting stack last 2 question left

--- Assingnments/A9_stack_array.cpp
+++ Assingnments/A9_stack_array.cpp
@@ -1,7 +1,55 @@
 #include<iostream>
 #include "A1st.cpp"
 using namespace std;
-class stack : private Array
+class Stack : private Array
 {
-    
+    public:
+        Stack(int);
+        void push(int);
+        int peek();
+        void pop();
+        ~Stack();
+        virtual bool isFull();
+        virtual bool isEmpty();
 };
+bool Stack::isEmpty(){
+    return Array::isEmpty();
+}
+bool Stack::isFull(){
+    return Array::isFull();
+}
+Stack::~Stack() {}
+void Stack::pop(){
+    if(isEmpty())
+        cout<<"Stack Underflow\n";
+    else
+        del(count()-1);
+}
+int Stack::peek(){
+    if(!isEmpty())
+        return get(count()-1);
+    else
+        cout<<"Stack is empty";
+    return 0;
+}
+void Stack::push(int data){
+    if(isFull())
+        cout<<"Stack OverFlow\n";
+    else
+        append(data);
+}
+Stack::Stack(int cap):Array(cap){ }
+
+int main()
+{
+    Stack s(10);
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    s.push(60);
+    s.push(70);
+    cout<<"last element of stack is : "<<s.peek()<<endl;
+}
