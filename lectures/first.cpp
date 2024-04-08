abbd8bba387bb4ab7c6210c6c0f2bf34cd6b886e
nikhil09 <nikhilside72@gmail.com>
2024-04-08 14:03:18 +0530

    star pattern

--- lectures/first.cpp
+++ lectures/first.cpp
@@ -1,14 +1,27 @@
 #include<iostream>
 using namespace std;
+void Rstar(){
+    int n;
+    cout<<"Enter the number of Row: "<<endl;
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+    cout<<" ";
+    cout<<endl;
+    }
+}
 int main()
 {
     for(int i=0; i<=6; i++){
-        for(int j=i-1; j<6; j++){
+        for(int j=i+1; j<6; j++){
             cout<<"* ";
         }
         cout<<" ";
         cout<<endl;
     }
+    Rstar();
     
     return 0;
 }
